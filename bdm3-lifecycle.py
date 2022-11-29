5bc129d146f50c6543a070d7e2c918efd3b870de
YueningJia <93624809+JJJJjyn@users.noreply.github.com>
2022-11-29 01:22:07 +0100

    Update bdm3-lifecycle.py

--- bdm3-lifecycle.py
+++ bdm3-lifecycle.py
@@ -2,7 +2,7 @@ import pandas as pd
 import mlflow
 
 #mlflow.set_tracking_uri("http://localhost:5000")
-#mlflow.set_tracking_uri("http://training.itu.dk:5000/")
+mlflow.set_tracking_uri("http://training.itu.dk:5000/")
 mlflow.set_experiment("yuji-BDM3")
 
 from sklearn.compose import ColumnTransformer
